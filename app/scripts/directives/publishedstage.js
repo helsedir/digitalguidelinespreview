3b726099db253b201ed4c2cbc3d4ec4258f0c772
Sjur Seibt <sjurlur@gmail.com>
2014-10-27 10:32:41 +0100

    add publishedSTage directive

--- app/scripts/directives/publishedstage.js
+++ app/scripts/directives/publishedstage.js
@@ -6,13 +6,27 @@
  * @description
  * # publishedStage
  */
-angular.module('angularApp')
-  .directive('publishedStage', function () {
+angular.module('guidelinePreviewApp')
+  .directive('publishedStage', function ($parse) {
     return {
-      template: '<div></div>',
+      template: '<div class="publishedstage {{publishedStage}}">{{resource}} er {{publishedStageText}}</div>',
       restrict: 'E',
+      scope: { 'publishedStage': '=publishedstage', 'resource': '@' },
       link: function postLink(scope, element, attrs) {
-        element.text('this is the publishedStage directive');
+      	scope.$watch('publishedStage', function() { 
+      		switch(scope.publishedStage){
+      			case 0:
+      				scope.publishedStageText = "kladd";
+      				break;
+      			case 1:
+      				scope.publishedStageText = "sent på høring";
+      				break;
+      			case 2:
+      				scope.publishedStageText = "publisert";
+      				break;
+      		}
+
+      	 });
       }
     };
   });
