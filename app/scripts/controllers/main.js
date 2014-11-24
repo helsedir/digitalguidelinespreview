49f6510a36872784ff2913e60c664da0b0015d90
Sjur Seibt <sjurlur@gmail.com>
2014-11-24 12:53:38 +0100

    add filtering of guidelines

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,10 +9,31 @@
  */
 angular.module('guidelinePreviewApp')
   .controller('MainCtrl', ['$scope', 'Guideline', 'toastr', 'ErrorService', function ($scope, Guideline, toastr, ErrorService) {
+    $scope.refineSearch = 0;
     Guideline.query().$promise.then(function(guidelines){
       		//success
     		$scope.guidelines = guidelines;
       	}, function(error){
       		ErrorService.handleError(error);
       	});
-  }]);
+  }])
+  .filter('isType', function () {
+    return function (items, type) {
+      //Check that the async items are received before filtering
+      if (typeof(items) != 'undefined') {
+        //If we want all items
+        if(type == 0) {
+          return items;
+        }
+        else {
+          var filtered = [];
+          for (var i = 0; i < items.length; i++) {
+            if (items[i].type == type) {
+              filtered.push(items[i]);
+            }
+          }
+          return filtered;
+        }
+      }
+    };
+  });
